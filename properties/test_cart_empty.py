0a07f5fca6b1b0e8aeff81a6e946c47c64a7add8
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-06-07 21:05:01 +0100

    add record_property, record_testsuite_property and generate junit xml report

--- properties/test_cart_empty.py
+++ properties/test_cart_empty.py
@@ -8,16 +8,11 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import pytest
 
-# define status of test
-status = "failed"
 
 def test_go_to_cart(driver, record_property, record_testsuite_property):
-    # assign that status variable is global
-    global status
     # save global properties for test suite
-    record_testsuite_property("Project","Play-Pro-V3")
+    record_testsuite_property("Project", "Play-Pro-V3")
     record_testsuite_property("Environment", "QA")
     record_testsuite_property("Test Suite", "Cart Suite Test")
     # save private properties of test
@@ -47,7 +42,3 @@ def test_go_to_cart(driver, record_property, record_testsuite_property):
     sleep(8)
     # assertion of url page
     assert (driver.current_url == "https://demotenant.playpro.fr/Panier") is True
-    # set status of test
-    status = "success"
-    # save private properties of test
-    record_property("Status", status)
