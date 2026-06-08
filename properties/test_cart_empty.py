54239a97c4bec7d30ee65819c8abc5856e3ff96a
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-06-09 00:59:35 +0100

    run jenkins test

--- properties/test_cart_empty.py
+++ properties/test_cart_empty.py
@@ -1,16 +1,16 @@
 """
 TC01: Go to panier using properties Pytest
-               and using record_property fixture
+               and using {record_property, record_testsuite_property} fixtures
 """
 from time import sleep
-
 from selenium.webdriver import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from utils.waits import Wait
+from utils.Logger import Logger
 
 
-def test_go_to_cart(driver, record_property, record_testsuite_property):
+def test_go_to_cart(driver, request, record_property, record_testsuite_property):
     # save global properties for test suite
     record_testsuite_property("Project", "Play-Pro-V3")
     record_testsuite_property("Environment", "QA")
@@ -20,25 +20,16 @@ def test_go_to_cart(driver, record_property, record_testsuite_property):
     record_property("Priority", "High")
     record_property("Browser", "Chrome")
     record_property("Type", "Functional")
-    # Go to home page
-    driver.get("https://demotenant.playpro.fr/")
-    # save a private property of test
     record_property("URL", driver.current_url)
-    # wait initialization
-    wait = WebDriverWait(driver, 15)
     # define Actions class
     actions = ActionChains(driver)
-    # close the cookie
-    cookie_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div/div/div[4]/div/button[1]")))
-    actions.move_to_element(cookie_btn)
-    actions.click(cookie_btn)
-    actions.perform()
     # locate panier web element
-    panier_icon = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@class='relative']")))
+    panier_icon = Wait.wait(driver).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='relative']")))
     actions.move_to_element(panier_icon)
     actions.click(panier_icon)
     actions.perform()
-    # wait for 8s to allow to load page
-    sleep(8)
+    # wait for 10s to allow to load page
+    sleep(10)
     # assertion of url page
     assert (driver.current_url == "https://demotenant.playpro.fr/Panier") is True
+    Logger.set_message(request.node.name + " OK")
