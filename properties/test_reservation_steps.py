e3c71c6b6e2c21c223ca1e25381e4ce0e9d42f58
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-06-10 00:34:26 +0100

    run first test of reservation activity cycle

--- properties/test_reservation_steps.py
+++ properties/test_reservation_steps.py
@@ -1,8 +1,7 @@
 """
-TC01: User should go to One Activity and redirect to reservation page
-      using record_property and record_testsuite_property fixtures
-
-      We identify 'Padel Court Test Tarifs Spécifiques' as an activity
+TC01: access to activity reservation page and follow all steps, finally our goal
+      is to create a reservation
+      for example: 'Padel Court Test Tarifs Spécifiques'
 """
 from time import sleep
 from utils.Logger import Logger
@@ -11,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
-def test_discover_activity(driver, request, record_property, record_testsuite_property):
+def test_reservation_steps(driver, request, record_property, record_testsuite_property):
     # save global properties for test suite
     record_testsuite_property("Project", "Play-Pro-V3")
     record_testsuite_property("Environment", "QA")
@@ -32,8 +31,20 @@ def test_discover_activity(driver, request, record_property, record_testsuite_pr
     driver.execute_script("arguments[0].click();", activity_book_btn)
     Logger.set_message("Activity is clicked")
     # wait for time to load page
-    sleep(20)
+    sleep(10)
+    # Locate Continue btn and click on it
+    continue_btn = Wait.wait(driver).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/button[1]")))
+    continue_btn.click()
+    # Locate Slot and click on it
+    slot_07_30 = Wait.wait(driver).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[2]/div[4]/div[2]/div[2]/button")))
+    driver.execute_script("arguments[0].scrollIntoView();", slot_07_30)
+    sleep(3)
+    driver.execute_script("arguments[0].click();", slot_07_30)
+    # Locate confirm my reservation btn and click on it
+    confirm_btn = Wait.wait(driver).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/button[1]")))
+    driver.execute_script("arguments[0].click();", confirm_btn)
+    # Wait Time to load page payment
+    sleep(10)
     # assertion
-    assert (driver.current_url == "https://demotenant.playpro.fr/discover/reservation/padel-court-test-tarifs-specifiques") is True
-    assert "padel-court-test-tarifs-specifiques" in driver.current_url
+    assert (driver.current_url == "https://demotenant.playpro.fr/Panier") is True
     Logger.set_message(request.node.name + " OK")
