41a7bc2e0e053441d0e4098ecf4b60fef9fb856f
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-06-11 00:07:28 +0100

    run sample test of reservation cycle

--- properties/test_reservation_steps.py
+++ properties/test_reservation_steps.py
@@ -3,6 +3,7 @@ TC01: access to activity reservation page and follow all steps, finally our goal
       is to create a reservation
       for example: 'Padel Court Test Tarifs Spécifiques'
 """
+import random
 from time import sleep
 from utils.Logger import Logger
 from utils.waits import Wait
@@ -22,12 +23,10 @@ def test_reservation_steps(driver, request, record_property, record_testsuite_pr
     record_property("Type", "Functional")
     record_property("URL", driver.current_url)
     # Locate Book Link in home page and click on it
-    book_link = Wait.wait(driver).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/main/section[1]/div[3]/div[2]/a")))
+    book_link = Wait.wait(driver).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/main/section[1]/div[3]/div[2]/a")))
     book_link.click()
     # Locate Activity Card and click on it
-    activity_book_btn = Wait.wait(driver).until(EC.element_to_be_clickable(
-        (By.XPATH, "/html/body/div[1]/div/section[1]/section[1]/div[2]/div[1]/div/div[2]/div/div[2]/div")))
+    activity_book_btn = Wait.wait(driver).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/section[1]/section[1]/div[2]/div[3]/div/div[2]/div/div[2]/div")))
     driver.execute_script("arguments[0].scrollIntoView();", activity_book_btn)
     sleep(3)
     driver.execute_script("arguments[0].click();", activity_book_btn)
@@ -35,18 +34,39 @@ def test_reservation_steps(driver, request, record_property, record_testsuite_pr
     # wait for time to load page
     sleep(10)
     # Locate Continue btn and click on it
-    continue_btn = Wait.wait(driver).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/button[1]")))
+    continue_btn = Wait.wait(driver).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[3]/div/div[2]/button[1]")))
     continue_btn.click()
-    # Locate Slot and click on it
-    table = []
-    Wait.wait(driver).until(lambda d: len(
-        d.find_elements(By.XPATH, "//button[@type='button']//div[@class='flex justify-center items-center']")) > 0)
+    # wait for time to load page
+    sleep(5)
+    # Locate Continue btn and click on it
+    next_calendar_btn = Wait.wait(driver).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div/div/button[2]")))
+    next_calendar_btn.click()
+    # wait for time to load page
+    sleep(5)
+    # Define a table
+    available_slots = []
+    # Locate all Slots to click on it after that
+    Wait.wait(driver).until(lambda d: len(d.find_elements(By.XPATH, "//button[@type='button']//div[@class='flex justify-center items-center']")) > 0)
     slots = driver.find_elements(By.XPATH, "//button[@type='button']//div[@class='flex justify-center items-center']")
     for slot in slots:
-        table.append(slot.text)
+        if "Complet" not in slot.text:
+            available_slots.append(slot)
     # display text of table slots
-    for text in table:
-        Logger.set_message("slot: " + text)
-
+    for slot in available_slots:
+        Logger.set_message("slot: " + slot.text)
+    # Define the slot to be clicked
+    selected_slot = random.choice(available_slots)
+    # Execute with JavaScript
+    driver.execute_script("arguments[0].scrollIntoView(true);", selected_slot)
+    driver.execute_script("arguments[0].click();", selected_slot)
+    # wait for 5s
+    sleep(5)
+    # Locate confirm reservation btn and click on it
+    confirm_reservation = Wait.wait(driver).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[3]/div/div/button[1]")))
+    driver.execute_script("arguments[0].click();", confirm_reservation)
+    # wait for 10s
+    sleep(10)
+    # assertion
+    assert (driver.current_url == "https://demotenant.playpro.fr/Panier") is True
+    ### print the status of Test
     Logger.set_message(request.node.name + " OK")
