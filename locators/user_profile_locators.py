1cd52e38b706fef1f22b57613e2d86f28d0b83ad
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-06-14 21:35:38 +0100

    Run Github Actions: test_reservation_03

--- locators/user_profile_locators.py
+++ locators/user_profile_locators.py
@@ -6,5 +6,5 @@ from selenium.webdriver.common.by import By
 
 class Locator(object):
     # user icon profile locator
-    user_profile_icon = (By.XPATH, "/html/body/header/div/div[2]/div/button/svg")
+    user_profile_icon = (By.XPATH, "/html/body/header/div/div[2]/div/button/div")
     my_reservations_link = (By.LINK_TEXT, "Mes réservations")
--- pages/user_profile_page.py
+++ pages/user_profile_page.py
@@ -10,7 +10,7 @@ class UserProfile(object):
         self.my_reservations_link = Wait.wait(driver).until((EC.element_to_be_clickable(Locator.my_reservations_link)))
 
     def click_user_profile_icon(self):
-        self.driver.execute_script("arguments[0].click();", self.user_profile_icon.click())
+        self.driver.execute_script("arguments[0].click();", self.user_profile_icon)
 
     def click_my_reservations_link(self):
         self.driver.execute_script("arguments[0].click();", self.my_reservations_link)
