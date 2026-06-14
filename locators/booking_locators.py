760692c445b8d82d467a9cae0795b4223c3b702d
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-06-14 20:49:30 +0100

    Run Github Actions: test_reservation_02

--- locators/booking_locators.py
+++ locators/booking_locators.py
@@ -6,4 +6,4 @@ from selenium.webdriver.common.by import By
 class Locator(object):
     # booking home page button
     booking_link = (By.LINK_TEXT, "Réserver")
-    booking_nav_link = (By.XPATH, "Réserver en Ligne")
+    booking_nav_link = (By.LINK_TEXT, "Réserver en Ligne")
