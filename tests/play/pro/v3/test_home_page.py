f164c875e5c545ab1d7eab95976f445576ff93d7
CH-SOF-GITHUB <bchaker28@yahoo.com>
2026-06-21 16:12:43 +0100

    set up environment variables for Allure report & Check Error for Categories

--- tests/play/pro/v3/test_home_page.py
+++ tests/play/pro/v3/test_home_page.py
@@ -18,7 +18,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 def test_open_home_page(driver: WebDriver):
     # Open Play Pro V3 home page
     driver.get("https://demotenant.playpro.fr/")
-    expected_title = "DEMO TENANTT"
+    expected_title = "DEMO TENANT"
     actual_title = driver.title
     Logger.set_message("Actual Title = " + actual_title)
     # pytest assert
